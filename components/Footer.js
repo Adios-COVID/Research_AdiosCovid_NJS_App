ca3d21b521ef114123403f3baf1017cdeb22d3f3
jdondy <jerdondoyano@gmail.com>
2023-02-28 11:34:48 -0800

    Added privacy statement/TOS links

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from "next/link";
+//import privacy from '../public/Adios COVID Research Privacy Statement.pdf'
 
 export default function Footer() {
   return (
@@ -10,6 +11,8 @@ export default function Footer() {
             <Link href="https://adioscovid.org/" target="_blank" rel="noopener noreferrer" className="text-danger adio_header">¡Adiós COVID!</Link>
             <div id="copyright" class="copyright">
               <i class="fa fa-copyright fa-1x"> 2023 ¡Adiós COVID!. All rights reserved</i>
+              <ul class="list-group"><a href="https://docs.google.com/document/u/3/d/e/2PACX-1vTwiQau-iw1LdAOSANsa9LYFZz3QosAb1zk14hdVdFgtjNF6gqD0E3ew0eI9l7Egt7WGe46_v41tXtb/pub" target="_blank" rel="noopener noreferrer">Privacy Statement</a></ul>
+              <ul class="list-group"><a href="https://docs.google.com/document/d/e/2PACX-1vT1UvCf9BD6GzOzMN8AwnPXq_TJs4fULe6p8bKvIuCPjrXVgjRTb5izWzsDLeMtVMkRDoAcgVJ7Bj3Z/pub" target="_blank" rel="noopener noreferrer">Terms of Service</a></ul>
             </div>
           </div>
           <div class="col-sm-4">
